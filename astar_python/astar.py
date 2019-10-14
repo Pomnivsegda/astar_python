c694cfd353b050536d21cd38be75dedcceb9ca80
Sergey Tyurin <pomnivsegda@gmail.com>
2019-10-14 16:35:48 +0300

    Add optional move_diag

--- astar_python/astar.py
+++ astar_python/astar.py
@@ -1,6 +1,7 @@
 class Astar:
 
-    def __init__(self, matrix):
+    def __init__(self, matrix, move_diag=False):
+        self.move_diag = move_diag
         self.mat = self.prepare_matrix(matrix)
 
     class Node:
@@ -35,25 +36,29 @@ class Astar:
 
     def neighbours(self, matrix, current):
         neighbours_list = []
-        if current.x - 1 >= 0 and current.y - 1 >= 0 and matrix[current.y - 1][current.x - 1].weight is not None:
-            neighbours_list.append(matrix[current.y - 1][current.x - 1])
+        
+        if self.move_diag:
+            if current.x - 1 >= 0 and current.y - 1 >= 0 and matrix[current.y - 1][current.x - 1].weight is not None:
+                neighbours_list.append(matrix[current.y - 1][current.x - 1])
+            if current.x - 1 >= 0 and current.y + 1 < len(matrix) and matrix[current.y + 1][
+                current.x - 1].weight is not None:
+                neighbours_list.append(matrix[current.y + 1][current.x - 1])
+            if current.x + 1 < len(matrix[0]) and current.y - 1 >= 0 and matrix[current.y - 1][
+                current.x + 1].weight is not None:
+                neighbours_list.append(matrix[current.y - 1][current.x + 1])
+            if current.x + 1 < len(matrix[0]) and current.y + 1 < len(matrix) and matrix[current.y + 1][
+                current.x + 1].weight is not None:
+                neighbours_list.append(matrix[current.y + 1][current.x + 1])
+
         if current.x - 1 >= 0 and matrix[current.y][current.x - 1].weight is not None:
             neighbours_list.append(matrix[current.y][current.x - 1])
-        if current.x - 1 >= 0 and current.y + 1 < len(matrix) and matrix[current.y + 1][
-            current.x - 1].weight is not None:
-            neighbours_list.append(matrix[current.y + 1][current.x - 1])
         if current.y - 1 >= 0 and matrix[current.y - 1][current.x].weight is not None:
             neighbours_list.append(matrix[current.y - 1][current.x])
         if current.y + 1 < len(matrix) and matrix[current.y + 1][current.x].weight is not None:
-            neighbours_list.append(matrix[current.y + 1][current.x])
-        if current.x + 1 < len(matrix[0]) and current.y - 1 >= 0 and matrix[current.y - 1][
-            current.x + 1].weight is not None:
-            neighbours_list.append(matrix[current.y - 1][current.x + 1])
+            neighbours_list.append(matrix[current.y + 1][current.x])            
         if current.x + 1 < len(matrix[0]) and matrix[current.y][current.x + 1].weight is not None:
             neighbours_list.append(matrix[current.y][current.x + 1])
-        if current.x + 1 < len(matrix[0]) and current.y + 1 < len(matrix) and matrix[current.y + 1][
-            current.x + 1].weight is not None:
-            neighbours_list.append(matrix[current.y + 1][current.x + 1])
+            
         return neighbours_list
 
     def build(self, end):
